c6ee23221ff942df43f23bfe9d0c185a38be3fd5
luisemi3 <70342476+luisemi3@users.noreply.github.com>
2020-09-10 14:08:48 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(feet)
+    basic.showString("Feet")
+    basic.showString("=")
+    yards = feet / 3
+    basic.showNumber(yards)
+    basic.showString("Yards")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(feet)
+    basic.showString("Feet")
+    basic.showString("=")
+    inches = feet * 12
+    basic.showNumber(inches)
+    basic.showString("Inches")
+    basic.showString("=")
+    centimeters = inches * 2.54
+    basic.showNumber(centimeters)
+    basic.showString("Centimeters")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(feet)
+    basic.showString("Feet")
+    basic.showString("=")
+    inches = feet * 12
+    basic.showNumber(inches)
+    basic.showString("Inches")
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(feet)
+    basic.showString("Feet")
+    basic.showString("=")
+    inches = feet * 12
+    basic.showNumber(inches)
+    basic.showString("Inches")
+    basic.showString("=")
+    centimeters = inches * 2.54
+    basic.showNumber(centimeters)
+    basic.showString("Centimeters")
+    basic.showString("=")
+    meters = centimeters / 100
+    basic.showNumber(meters)
+    basic.showString("Meters")
+})
+let meters = 0
+let centimeters = 0
+let inches = 0
+let yards = 0
+let feet = 0
+feet = randint(1, 100)
+basic.forever(function () {
+	
+})
